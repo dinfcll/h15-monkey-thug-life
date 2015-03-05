fc0b58a005ee549f7ee1144baa5d8f1d2039cbae
Joyvax <jonathan.dechelle@hotmail.com>
2015-03-04 19:59:24 -0600

    Restructuration fichier decor

--- FinalProjet/FinalProject/GestionDecor/Nuage.h
+++ FinalProjet/FinalProject/GestionDecor/Nuage.h
@@ -7,5 +7,15 @@ class Nuage
 public:
 	Nuage(void);
 	~Nuage(void);
+	Nuage(int Positionx, int Positiony,float Speed, Ressource Ressources);
+	SDL_Surface* TabNuageTexture; 
+	int Positionx, Positiony;
+	SDL_Rect* RecDestination;
+	SDL_Rect* RecDestination2;
+	SDL_Rect* RecDestination3;
+	float Speed;
+
+	void Update();
+	void Draw(SDL_Surface* &SurfaceDessin);
 };
 
